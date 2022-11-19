d24b58d415bce6e389d8b73ecc5bd0e77f835931
RocioAldanaMendez <rocioalmendez@gmail.com>
2022-11-19 17:32:53 -0300

    Update Greet_in_many_languages.py
    segundo cambio

--- Greet_in_many_languages.py
+++ Greet_in_many_languages.py
@@ -19,3 +19,22 @@ lenguage = input()
 
 print(mi_diccionario[lenguage])
 
+print("Comprobando subir cambios desdes github desktop")
+
+complex(4, 5)
+(4+5j)
+
+complex(6, 8)
+(6+8j)
+
+complex(3.4, 3.4)
+(3.4+3.4j)
+
+complex(0, 0)
+0j
+
+complex(5)
+(5+0j)
+
+complex(0, 4)
+4j
